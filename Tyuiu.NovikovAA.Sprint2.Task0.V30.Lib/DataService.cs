8f19e11931f96634d277b96acc8d6eaf9e4e962d
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-11-04 23:03:02 +0600

    Task0

--- Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib
             bool[] res = new bool[6];
 
             res[0] = x + 1640 == y;
-            res[1] = x + 1 != y;
+            res[1] = x + 1640 != y;
             res[2] = x + 1 < y;
             res[3] = x + 1 > y;
             res[4] = x + 1 <= y;

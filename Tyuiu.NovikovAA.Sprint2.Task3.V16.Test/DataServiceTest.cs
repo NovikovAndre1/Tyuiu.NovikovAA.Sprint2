0c061998c7af7c7bab1624432d18cbb4c415f5ad
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-11-15 00:21:19 +0600

    Task3

---
+++ Tyuiu.NovikovAA.Sprint2.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,50 @@
+
+using Tyuiu.NovikovAA.Sprint2.Task3.V16.Lib;
+
+
+namespace Tyuiu.NovikovAA.Sprint2.Task3.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 29;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -21;
+            double res = ds.Calculate(x);
+            double wait = -230.952;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NovikovAA.Sprint2.Task3.V16/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Tyuiu.NovikovAA.Sprint2.Task3.V16.Lib;
+
+
+namespace Tyuiu.NovikovAA.Sprint2.Task3.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("Исходные данные");
+            Console.WriteLine("********************************************************************");
+
+            Console.WriteLine("Введите значение х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ");
+            Console.WriteLine("********************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}

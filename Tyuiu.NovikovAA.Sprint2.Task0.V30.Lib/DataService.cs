cbbde083b5fa3304b19bee3af99be775e9410b32
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-11-21 01:37:47 +0600

    Task0

--- Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Reflection.Metadata.Ecma335;
 using tyuiu.cources.programming.interfaces.Sprint2;
 
 
@@ -10,12 +11,12 @@ namespace Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib
         {
             bool[] res = new bool[6];
 
-            res[0] = x + 1640 == y;
-            res[1] = x + 1640 != y;
-            res[2] = x + 1 < y;
-            res[3] = x + 1 > y;
-            res[4] = x + 1 <= y;
-            res[5] = x + 1 >= y;
+            res[0] = x + 1 != y;          // 96 != 1735 (True)
+            res[1] = x == y;              // 95 == 1735 (False)
+            res[2] = x < y;               // 95 < 1735  (True)
+            res[3] = x + 2 <= y;          // 97 <= 1735 (True) - изменён оператор для получения False
+            res[4] = x <= y;              // 95 <= 1735 (True)
+            res[5] = x + 3 < y;           // 98 < 1735  (True) - изменён оператор для получения False
 
             return res;
         }

bcd1f374089fea9e388a3d02cfe480444d53249c
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-11-21 01:39:58 +0600

    Task0

--- Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib/DataService.cs
@@ -11,12 +11,14 @@ namespace Tyuiu.NovikovAA.Sprint2.Task0.V30.Lib
         {
             bool[] res = new bool[6];
 
-            res[0] = x + 1 != y;          // 96 != 1735 (True)
-            res[1] = x == y;              // 95 == 1735 (False)
-            res[2] = x < y;               // 95 < 1735  (True)
-            res[3] = x + 2 <= y;          // 97 <= 1735 (True) - изменён оператор для получения False
-            res[4] = x <= y;              // 95 <= 1735 (True)
-            res[5] = x + 3 < y;           // 98 < 1735  (True) - изменён оператор для получения False
+            
+
+            res[0] = x + 1 != y;
+            res[1] = x == y;
+            res[2] = x < y;
+            res[3] = x + 2 <= y;
+            res[4] = x <= y;
+            res[5] = x + 3 < y;
 
             return res;
         }
